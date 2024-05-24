4dc46e0abc280d512e95f980caa51bf45a5e3bde
Antonio <antonio_pereira74@hotmail.com>
2024-05-23 23:15:02 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Calculadora de IMC no React
+
+O parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa é o IMC (índice de massa córporea).<br>
+
+Cálculo do índice:  
+IMC = peso / (altura x altura)<br>
+
+O aplicativo desenvolvido em React oferece a funcionalidade de coletar os dados de peso e altura do usuário e pressionando o botão 'Calcular', é exibido uma tabela com o resultado do índice e situação atual conforme a tabela de classificação das faixas de IMC.<br>
+
+<img src='./public/img.png'><br>
+
+### TECNOLOGIAS:<br>
+HTML<br>
+CSS<br>
+REACT<br>
+GIT/GITHUB
+
+
+
+
+
---
+++ src/components/ImcCalc.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import Button from './Button'
+import './ImcCalc.css'
+
+const ImcCalc = () => {
+    const [height, setHeight] = useState('')
+    const [weight, setWeight] = useState('')
+
+    const clearForm = (e) => {
+        e.preventDefault()
+        setWeight = ('')
+        setHeight = ('')
+        
+    }
+
+  return (
+    <div id='calc-container'>
+        <h2>Calculadora de IMC</h2>
+
+        <form id="imc-form">
+            <div className="form-inputs">
+                <div className="form-control">
+                    <label htmlFor="hight">Altura:</label>
+                    <input 
+                        type="text" 
+                        name='height'
+                        id='height'
+                        placeholder='Exemplo 1,75'
+                        onChange={(e) => setHeight(e.target.value)}
+                        value={height}
+                    />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="weight">Peso:</label>
+                    <input 
+                        type="text" 
+                        name='weight'
+                        id='weight'
+                        placeholder='Exemplo 70,5'
+                        onChange={(e) => setWeight(e.target.value)}
+                        value={weight}
+                    />
+                </div>
+            </div>
+
+            <div className="action-control">
+                <Button id='calc-btn' text='Calcular'/>
+                <Button id='clear-btn' text='Limpar' action={clearForm}/>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default ImcCalc
